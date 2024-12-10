f5c188e5dd6241f0860483705827cfebc40e59e1
Gareth Powell <gareth@ziniki.org>
2024-12-10 14:53:57 +0000

    added tests to drive sorting signatories

---
+++ internal/api/transaction_test.go
@@ -0,0 +1,36 @@
+package api_test
+
+import (
+	"net/url"
+	"testing"
+
+	"github.com/gmmapowell/ChainLedger/internal/api"
+)
+
+func TestTwoSignaturesAddedInCollatingOrderStayThatWay(t *testing.T) {
+	tx, _ := api.NewTransaction("https://test.com", []byte("hashcode"))
+	u1, _ := url.Parse("http://user1.com")
+	tx.Signer(u1)
+	u2, _ := url.Parse("http://user2.com")
+	tx.Signer(u2)
+	if tx.Signatories[0].Signer.Host != "user1.com" {
+		t.Fatalf("the first signer was %s\n", tx.Signatories[0].Signer)
+	}
+	if tx.Signatories[1].Signer.Host != "user2.com" {
+		t.Fatalf("the second signer was %s\n", tx.Signatories[1].Signer)
+	}
+}
+
+func TestTwoSignaturesAddedInInverseCollatingOrderAreReversed(t *testing.T) {
+	tx, _ := api.NewTransaction("https://test.com", []byte("hashcode"))
+	u2, _ := url.Parse("http://user2.com")
+	tx.Signer(u2)
+	u1, _ := url.Parse("http://user1.com")
+	tx.Signer(u1)
+	if tx.Signatories[0].Signer.Host != "user1.com" {
+		t.Fatalf("the first signer was %s\n", tx.Signatories[0].Signer)
+	}
+	if tx.Signatories[1].Signer.Host != "user2.com" {
+		t.Fatalf("the second signer was %s\n", tx.Signatories[1].Signer)
+	}
+}
