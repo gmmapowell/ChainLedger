e63d218878eedf1dea431da40f8c6142f9ebd970
Gareth Powell <gareth@ziniki.org>
2024-12-11 11:00:31 +0000

    Introducing a clock double

---
+++ internal/helpers/clock.go
@@ -0,0 +1,59 @@
+package helpers
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gmmapowell/ChainLedger/internal/types"
+)
+
+type Clock interface {
+	Time() types.Timestamp
+}
+
+type ClockDouble struct {
+	Times []types.Timestamp
+	next  int
+}
+
+func ClockDoubleIsoTimes(isoTimes ...string) ClockDouble {
+	ts := make([]types.Timestamp, len(isoTimes))
+	for i, s := range isoTimes {
+		ts[i], _ = types.ParseTimestamp(s)
+	}
+	return ClockDouble{Times: ts, next: 0}
+}
+
+func ClockDoubleSameDay(isoDate string, times ...string) ClockDouble {
+	ts := make([]types.Timestamp, len(times))
+	for i, s := range times {
+		ts[i], _ = types.ParseTimestamp(isoDate + "_" + s)
+	}
+	return ClockDouble{Times: ts, next: 0}
+}
+
+func ClockDoubleSameMinute(isoDateHM string, seconds ...string) ClockDouble {
+	ts := make([]types.Timestamp, len(seconds))
+	for i, s := range seconds {
+		ts[i], _ = types.ParseTimestamp(isoDateHM + ":" + s)
+	}
+	return ClockDouble{Times: ts, next: 0}
+}
+
+func (clock *ClockDouble) Time() types.Timestamp {
+	if clock.next > len(clock.Times) {
+		panic("more timestamps requested than provided")
+	}
+	r := clock.Times[clock.next]
+	clock.next++
+	return r
+}
+
+type ClockLive struct {
+}
+
+func (clock *ClockLive) Time() types.Timestamp {
+	gotime := time.Now().UnixMilli()
+	fmt.Printf("%d", gotime)
+	return types.Timestamp(gotime)
+}
--- internal/types/timestamp.go
+++ internal/types/timestamp.go
@@ -1,3 +1,21 @@
 package types
 
+import (
+	"time"
+)
+
 type Timestamp int64
+
+const IsoFormat = "2006-01-02_15:04:05.999"
+
+func ParseTimestamp(iso string) (Timestamp, error) {
+	ts, err := time.Parse(IsoFormat, iso)
+	if err != nil {
+		return Timestamp(0), err
+	}
+	return Timestamp(ts.UnixMilli()), nil
+
+}
+func (ts Timestamp) IsoTime() string {
+	return time.UnixMilli(int64(ts)).Format(IsoFormat)
+}
