66e232068ead03f2d864e842509c2d065c8451ec
Gareth Powell <gareth@ziniki.org>
2024-12-12 18:00:00 +0000

    Adding code to submit a transaction in Begin

--- internal/harness/api.go
+++ internal/harness/api.go
@@ -1,6 +1,11 @@
 package harness
 
 import (
+	"fmt"
+	"hash/maphash"
+	"log"
+
+	"github.com/gmmapowell/ChainLedger/internal/api"
 	"github.com/gmmapowell/ChainLedger/internal/client"
 	"github.com/gmmapowell/ChainLedger/internal/clienthandler"
 )
@@ -18,7 +23,28 @@ type ConfigClient struct {
 }
 
 func (cli *ConfigClient) Begin() {
+	go func() {
+		var hasher maphash.Hash
+		hasher.WriteString("hello, world")
+		h := hasher.Sum(nil)
 
+		tx, err := api.NewTransaction("http://tx.info/msg1", h)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		err = tx.SignerId("https://user2.com")
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		err = cli.submitter.Submit(tx)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		fmt.Printf("submitted transaction: %v", tx)
+	}()
 }
 
 func (cli *ConfigClient) WaitFor() {
