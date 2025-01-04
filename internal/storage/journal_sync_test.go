bdf1016dc7e6d1889c10b9dc077030ff272cce8e
Gareth Powell <gareth@ziniki.org>
2025-01-04 09:29:57 -0500

    Journal survives attempt to reallocate

--- internal/storage/journal_sync_test.go
+++ internal/storage/journal_sync_test.go
@@ -1,6 +1,7 @@
 package storage_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/gmmapowell/ChainLedger/internal/helpers"
@@ -9,13 +10,15 @@ import (
 )
 
 func TestWeCanAddAndRecoverAtTheSameTime(t *testing.T) {
+	clock := helpers.ClockDoubleSameMinute("2024-12-25_03:00", "05.121", "07.282", "11.281", "19.202")
+
 	finj := helpers.FaultInjectionLibrary(t)
 	tj := storage.TestJournaller("journal", finj)
 	journal := tj.(*storage.MemoryJournaller)
 	completed := false
 	go func() {
 		for !completed {
-			journal.RecordTx(storableTx())
+			journal.RecordTx(storableTx(clock))
 		}
 	}()
 	aw := finj.AllocatedWaiter()
@@ -23,9 +26,17 @@ func TestWeCanAddAndRecoverAtTheSameTime(t *testing.T) {
 		aw.Release()
 		aw = finj.AllocatedWaiter()
 	}
-
+	go func() {
+		txs, _ := tj.ReadTransactionsBetween(clock.Times[0], clock.Times[3])
+		fmt.Printf("%v\n", txs)
+	}()
+	rw := finj.AllocatedWaiter()
+	aw.Release()
+	/*aw = */ finj.AllocatedWaiter()
+	rw.Release()
+	// aw.Release()
 }
 
-func storableTx() *records.StoredTransaction {
-	return &records.StoredTransaction{TxID: []byte("hello")}
+func storableTx(clock helpers.Clock) *records.StoredTransaction {
+	return &records.StoredTransaction{TxID: []byte("hello"), WhenReceived: clock.Time()}
 }
